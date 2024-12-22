a670802d121e1d8f5f160a3bff5da9dfda3de80e
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2024-12-22 18:30:05 -0500

    Update Sign up and Login

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const signupBtn = document.getElementById('signup');
+const loginBtn = document.getElementById('login');
+
+const signupContainer = document.querySelector('.signup-container');
+const loginContainer = document.querySelector('.login-container');
+
+loginBtn.addEventListener('click', () => {
+    signupContainer.classList.add('slide-up');
+    loginContainer.classList.remove('slide-up')
+});
+
+signupBtn.addEventListener('click', () => {
+    loginContainer.classList.add('slide-up');
+    signupContainer.classList.remove('slide-up')
+});
